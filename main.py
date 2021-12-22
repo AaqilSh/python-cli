833d3cfe9b696274a7257d7ced7159c29f73f6ff
Aaqil Shihab <aaqilsh@yahoo.com>
2021-12-22 11:10:31 +0530

    Tested typer with color

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import click
 import typer
 
 app = typer.Typer()
@@ -6,6 +7,12 @@ app = typer.Typer()
 @app.command()
 def hello(name: str):
     typer.echo(f"Hello {name}", color=typer.colors.RED)
+
+
+@app.command()
+def add(num1: int, num2: int):
+    sum = num1+num2
+    typer.echo(typer.style(num1+num2, fg=typer.colors.RED))
 
 
 if __name__ == '__main__':
