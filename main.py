05d24919ec016448d837de0fb7395937dba3f20a
Aaqil Shihab <aaqilsh@yahoo.com>
2021-12-25 17:46:59 +0530

    test

--- main.py
+++ main.py
@@ -27,20 +27,20 @@ def flutterr():
 
 @app.command()
 def dirr(create: bool = True, rename: bool = False):
-    # os.chdir('/home/aaqil/python/projects/experiments/')
-    # if create == True and rename == False:
-    #     name = typer.prompt('Enter the folder name')
-    #     subprocess.run(['mkdir', name])
-    #     subprocess.run(['ls'])
-    # else:
-    #     name = typer.prompt('Enter the folder name')
-    #     source = os.path.join(os.getcwd(), name)
-    #     tar = typer.prompt('Enter the new folder name')
-    #     tar = os.path.join(os.getcwd(), tar)
-    #     subprocess.run(['mv', source, tar])
-    #     subprocess.run(['ls'])
-    #     print(os.getcwd())
     os.chdir('/home/aaqil/python/projects/experiments/')
+    if create == True and rename == False:
+        name = typer.prompt('Enter the folder name')
+        subprocess.run(['mkdir', name])
+        subprocess.run(['ls'])
+    else:
+        name = typer.prompt('Enter the folder name')
+        source = os.path.join(os.getcwd(), name)
+        tar = typer.prompt('Enter the new folder name')
+        tar = os.path.join(os.getcwd(), tar)
+        subprocess.run(['mv', source, tar])
+        subprocess.run(['ls'])
+        print(os.getcwd())
+    # os.chdir('/home/aaqil/python/projects/experiments/')
     subprocess.run(['ls'])
 
 
